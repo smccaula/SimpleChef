21e9cfa121ff8b655ce519322f9110d62d1fb0ed
Scott McCaulay <scottmccaulay@gmail.com>
2014-08-07 15:23:31 -0400

    added most recipes

--- solo.rb
+++ solo.rb
@@ -1,3 +1,7 @@
 ﻿file_cache_path  "/home/smccaula/SimpleChef/"
 cookbook_path  "/home/smccaula/SimpleChef/cookbooks"
 data_bag_path  "/home/smccaula/SimpleChef/databags"
+
+  # Verify all HTTPS connections (recommended)
+  ssl_verify_mode :verify_peer
+
